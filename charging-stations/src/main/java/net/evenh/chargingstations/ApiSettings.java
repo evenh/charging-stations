b873b22d246ba78e03d401922a4db565a492caa7
Even Holthe <even.holthe@me.com>
2014-11-03 13:08:06 +0100

    Added API-key file

---
+++ charging-stations/src/main/java/net/evenh/chargingstations/ApiSettings.java
@@ -0,0 +1,11 @@
+package net.evenh.chargingstations;
+
+/**
+ * Nobil API-specific settings
+ *
+ * @author Even Holthe
+ * @since 1.0.0
+ */
+public class ApiSettings {
+    public static final String API_KEY = "Insert Key here";
+}
