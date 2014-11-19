7af263aa4a3c22abed514df55ebf753847f416b4
Even Holthe <even.holthe@me.com>
2014-11-19 13:52:30 +0100

    Updated ApiSettings

--- charging-stations/src/main/java/net/evenh/chargingstations/ApiSettings.java
+++ charging-stations/src/main/java/net/evenh/chargingstations/ApiSettings.java
@@ -7,5 +7,6 @@ package net.evenh.chargingstations;
  * @since 1.0.0
  */
 public class ApiSettings {
-    public static final String API_KEY = "Insert Key here";
+	public static final String API_BASE_URL = "http://nobil.deployapp.me/api";
+    public static final String API_KEY = "Insert API-key here";
 }
